f8f0720ed57e4f392541792026b45acab710ec0f
David <daviluquea@gmail.com>
2025-02-19 09:48:46 -0500

    Add files via upload

---
+++ Fisica/modelo_lineal.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class RegresionLineal: 
+    def __init__(self):
+        self.theta = None
+        self.intercept = None
+        self.coef = None
+        
+    def fit(self,x,y):
+        n = len(x)
+        x_b = np.c_[np.ones((n,1)),x]
+        self.theta = np.linalg.pinv(x_b.T.dot(x_b)).dot(x_b.T).dot(y)
+        self.intercept = self.theta[0]
+        self.coef = self.theta[1:]
